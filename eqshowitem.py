9ea93ac0040e4942bca27f1e363e8304d5f2b944
Tom <tfeckert@gmail.com>
2025-11-28 12:39:07 -0500

    eqshowitem: add option to pass filename as parameter, outputs description of every item in file

--- eqshowitem.py
+++ eqshowitem.py
@@ -1,6 +1,35 @@
 import mariadb
 import sys
 
+def show_item_stats(cursor, item_db_id, curslot=None):
+    cursor.execute(
+        "SELECT name, restrictions, primary_stats, stats, regens, resists "
+        "FROM Item WHERE id = ?",
+        (item_db_id,)
+    )
+    row = cursor.fetchone()
+    if row is None:
+        print(f"Item ID {item_db_id} not found in database.")
+        return
+    print(f"Item Name: {row[0]}")
+    if curslot is not None:
+        print(f"Cursor Slot: {curslot}")
+    print(f"Restrictions: {row[1]}")
+    print(f"Primary Stats: {row[2]}")
+    print(f"Stats: {row[3]}")
+    print(f"Regens: {row[4]}")
+    print(f"Resists: {row[5]}")
+
+def lookup_item_ids(cursor, name):
+    cursor.execute(
+        "SELECT id, allakhazam_id FROM Item WHERE name = ?",
+        (name,)
+    )
+    row = cursor.fetchone()
+    if row is not None:
+        return row[0], row[1]
+    return None, None
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("need to specify item to lookup\n")
@@ -13,15 +42,26 @@ if __name__ == "__main__":
             port=3306,
             database="everquest_data")
         cursor = conn.cursor()
-        cursor.execute(
-            "SELECT id, allakhazam_id FROM Item WHERE name = ?",
-            (sys.argv[1],)
-        )
-        row = cursor.fetchone()
-        if row is None:
-            print(f"Item not found in DB: {sys.argv[1]}")
-        else:
-            print(f"Item found in DB: {row[0]}: {row[1]}")
+        namesandslots = []
+        try:
+            with open(sys.argv[1], 'r') as f:
+                for line in f:
+                    nameslot = line.strip().split(',', 1)
+                    if len(nameslot) != 2:
+                        name = nameslot[0]
+                        curslot = None
+                    else:
+                        name, curslot = nameslot
+                    namesandslots.append((name, curslot))
+        except FileNotFoundError:
+            namesandslots.append((sys.argv[1], None))
+        print(namesandslots)
+        for name, curslot in namesandslots:
+            id,_ = lookup_item_ids(cursor, name)
+            if id is not None:
+                show_item_stats(cursor, id, curslot)
+            else:
+                print(f"Item '{name}' not found in database.")
         cursor.close()
         conn.close()
     except mariadb.Error as e:
